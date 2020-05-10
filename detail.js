c1ab9c2869b4ed5164009a189904d66d3656aaa4
hc3099 <hc3099@tc.columbia.edu>
2020-05-10 16:33:40 +0800

    adding slice function

--- detail.js
+++ detail.js
@@ -1,2 +1,7 @@
 const {href, search, searchParams} = new URL(location.href);
-console.log(href, search, searchParams)
+console.log(href, search, searchParams);
+let uid = getId(search);
+console.log(uid);
+function getId() {
+  return search.slice(4,6)
+}
